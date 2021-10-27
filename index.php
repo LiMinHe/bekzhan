7bf19037f0da6bd47e5ccd2c8898fc1411393f3b
LiMinHe <93245184+LiMinHe@users.noreply.github.com>
2021-10-27 14:07:05 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
      <style>
           body {
-               background: #573ed3 url(csc-20a/kyrgyzstanflag.jpg); /*Цвет фона и путь к файлу */
+               background: #574ed3 url(csc-20a/kyrgyzstanflag.jpg); /*Цвет фона и путь к файлу */
                color: #fff; /*Цвет текста*/
           }
           </style>
