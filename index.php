33b3d9b09985520417fb2d02fa31cd4ff32acac1
LiMinHe <93245184+LiMinHe@users.noreply.github.com>
2021-10-27 13:58:07 +0600

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
      
 </style>     
 <pre><h1 syle="text-align: center;">                                         my Portofolio:
-<pre><h2>Name:  Adinur</h2></pre>
+<pre><h2>Name:  Bekjan Myrzaev</h2></pre>
 <pre><h2>Info: I live in Bishkek. 17 years old</h2></pre>                  
-<pre><h2>Phone number: 055855998</h2></pre>
-
+<pre><h2>Phone number: 0501408508</h2></pre>
+<pre><h2>Hobby: I play football and basketball
 
 <pre><h1;">
 </body>
