9111dff0d2eb528318e6d3a99be12f9b4ce1271a
LiMinHe <93245184+LiMinHe@users.noreply.github.com>
2021-11-16 14:53:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,32 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+             background-color: rgb(116, 3, 3)(225, 55, 231); /*Цвет фона и путь к файлу */
+             color: rgb(0, 0, 0); /*Цвет текста*/
+        }
+        html{
+            width: 100%;
+            background-color: blue;
+        }
+        </style>
+   
+
+</head>
 <body>
-     <style>
-          body {
-               background-color: rgb(116, 3, 3)(225, 55, 231); /*Цвет фона и путь к файлу */
-               color: rgb(0, 0, 0); /*Цвет текста*/
-          }
-          html{
-              width: 100%;
-              background-color: blue;
-          }
-          </style>
-     <style>
-     .rihgtimg{
-           float: right;
-           marging: 7px 0 7px 7px;
-     }   
-     
-     
-     
-     
-     
-     
-     
-</style>     
-<pre><h1 syle="text-align: center;">                                        Portfolio:
-<pre><h2>Name:  Bekjan Myrzaev Bolotbekovich</h2></pre>
-<pre><h2>About me: I live in Bishkek. 17 years old. Born in Yssyk-Kul, 1st march 2004</h2></pre>                  
-<pre><h2>Phone number: 0501408508</h2></pre>
-<pre><h2>Hobby: I'm play football and basketball
-<img src="sisa.jpg" alt="Anime" class="mev">
-<pre><h1;">
+    <pre><h1 syle="text-align: center;">                                        Portfolio:
+        <pre><h2>Name:  Bekjan Myrzaev Bolotbekovich</h2></pre>
+        <pre><h2>About me: I live in Bishkek. 17 years old. Born in Yssyk-Kul, 1st march 2004</h2></pre>                  
+        <pre><h2>Phone number: 0501408508</h2></pre>
+        <pre><h2>Hobby: I'm play football and basketball
+        <img src="sisa.jpg" alt="Anime" class="mev">
+        <pre><h1;">
 </body>
 </html>
-     
+<html>
+<body>
